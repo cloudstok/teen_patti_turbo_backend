51af680b1c245e6609af3322dadd34853fc3c0f8
Chinmay Gawali <chinmay1gawali@gmail.com>
2025-07-18 16:08:57 +0530

    Added route and logic to retrieve the game history

--- src/modules/bets/betDb.ts
+++ src/modules/bets/betDb.ts
@@ -1,5 +1,5 @@
 import { Settlement } from "../../interface/interface"
-import { write } from "../../utils/dbConnection";
+import { write, read } from "../../utils/dbConnection";
 
 const SQL_INSERT_QUERY = 'INSERT INTO settlement (user_id, round_id, operator_id, bet_amount, winning_amount, multiplier, status, result) VALUES (?,?,?,?,?,?,?,?)'
 export const insertData = async(data:Settlement) =>{
@@ -19,4 +19,31 @@ export const insertData = async(data:Settlement) =>{
     }catch(err:any) {
         console.log('Error inserting the data :',err.message);
     }
-}
+};
+
+export const readData = async (user_id: string, operator_id: string, limit: number): Promise<any[] | undefined> => {
+    try {
+        const SQL_READ_QUERY = `select * from settlement where user_id = ? AND operator_id = ? order by created_at DESC LIMIT ${limit}`
+        const data = await read(SQL_READ_QUERY, [user_id, operator_id]);
+        if (!data) {
+            console.log(`Error from read data :Data not found for user_id : ${user_id} and operator_id : ${operator_id}`);
+            return 
+        } else {
+            const parsedData = data.map((row: any) => {
+            try {
+                row.result = JSON.parse(row.result);
+            } catch (err:any) {
+                console.warn(`Failed to parse result for row ID ${row.id}.  Message: ${err.message}`,);
+                row.result = []; 
+            }
+            return row;
+        });
+            console.log(`Data read successfully Player_id : ${user_id} and ${operator_id}`);
+            return parsedData
+        };
+
+    } catch (err: any) {
+        console.log('Error from read data :', err.message);
+    }
+
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,32 @@
 import { Router, Response } from "express";
+import { readData } from "../modules/bets/betDb";
 
 const routes = Router();
 
 routes.get('/', (_, res: Response) => {
     res.send({ status: true, msg: 'Teen Patti Turbo game tested successfully' });
+});
+
+routes.get('/games-history', async (req, res) => {
+    try {
+        const { user_id, operator_id, limit } = req.query as {
+            user_id: string;
+            operator_id: string;
+            limit?: string;
+        };
+        if (!user_id || !operator_id){
+            res.status(400).json({status : false, message: 'Required params : user_id and operator_id'});
+        };  
+
+        const parsedLimit = limit ? parseInt(limit,10): 10
+        const result = await readData(user_id, operator_id,parsedLimit);
+        if(!result || result.length === 0) {
+             res.status(404).json({status: false, data: result})
+        }
+         res.status(200).json({ status: true, data: result });
+    } catch (err: any) {
+         res.status(500).json({ status: false, message: 'Error fetching game history', error: err.message });
+    };
 });
 
 export { routes };
